34f6d8b4b7d8038049faea6aa5606c4505e56c0a
AdrieanKhisbe <adriean.khisbe@live.fr>
2016-01-06 19:34:07 +0100

    Setup linting

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var vorpal = require('vorpal')()
 
 vorpal
-    .command('hello [who]', 'Says Hello to "who"')
-    .action(function (args, callback) {
-        this.log('Hello ' + (args.who || 'Seneca') + '!');
-        callback();
-    })
+      .command('hello [who]', 'Says Hello to "who"')
+      .action(function (args, callback) {
+        this.log('Hello ' + (args.who || 'Seneca') + '!')
+        callback()
+      })
 
 vorpal
     .delimiter('[seneca]> ')
