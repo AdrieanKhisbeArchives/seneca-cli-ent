eb691814ce446cead8ceab1a51502e8ecdfea0db
AdrieanKhisbe <adriean.khisbe@live.fr>
2016-01-06 19:28:23 +0100

    Minimal vorpal Cli

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('seneca.act({“message": "Come back later for pure Awesomeness"});');
+var vorpal = require('vorpal')()
+
+vorpal
+    .command('hello [who]', 'Says Hello to "who"')
+    .action(function (args, callback) {
+        this.log('Hello ' + (args.who || 'Seneca') + '!');
+        callback();
+    })
+
+vorpal
+    .delimiter('[seneca]> ')
+    .show()
